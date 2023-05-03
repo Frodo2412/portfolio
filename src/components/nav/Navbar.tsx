c294726549455792e21a8bcc42b58edc73d3db0e
bruno.lemus <bruno.lemus@globant.com>
2023-05-03 19:46:13 -0300

    Add navbar

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,17 @@
+import './Navbar.scss'
+
+export default function Navbar() {
+
+    return (
+        <div id={"navbar"}>
+            <ul>
+                <li><a href={'#'}>Logo</a></li>
+                <li><a href={'#'}>Home</a></li>
+                <li><a href={'#'}>Technologies</a></li>
+                <li><a href={'#'}>Experience</a></li>
+                <li><a href={'#'}>Contact</a></li>
+            </ul>
+        </div>
+    )
+
+}
