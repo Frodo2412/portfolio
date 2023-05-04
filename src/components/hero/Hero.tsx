9ec812357f7c9e62a96d501a44403e63e07c1bb2
bruno.lemus <bruno.lemus@globant.com>
2023-05-03 21:49:09 -0300

    Add Hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,15 @@
+import './Hero.css'
+
 export default function Hero() {
 
-    return(
-        <div>Hero</div>
+    return (
+        <div className={"hero"}>
+            <div className={"hero-text-container"}>
+                <p className={"hero-text"}>
+                    Do you want to build <strong>efficient</strong> software that <strong>scales</strong>?
+                </p>
+            </div>
+        </div>
     )
 
 }
