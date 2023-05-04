74b48d375539765386ef52604a5305b93790c31a
bruno.lemus <bruno.lemus@globant.com>
2023-05-03 21:16:10 -0300

    Add logo

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,11 +1,12 @@
 import './Navbar.scss'
+import logo from '../../assets/logo.png'
 
 export default function Navbar() {
 
     return (
         <div id={"navbar"}>
             <ul>
-                <li><a href={'#'}>Logo</a></li>
+                <li><img src={logo} alt={"logo"} className={"logo"}/></li>
                 <li><a href={'#'}>Home</a></li>
                 <li><a href={'#'}>Technologies</a></li>
                 <li><a href={'#'}>Experience</a></li>
