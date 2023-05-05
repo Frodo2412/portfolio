7292ed88f1e994d28c3daef871e50a980ff00c8d
bruno.lemus <bruno.lemus@globant.com>
2023-05-05 20:27:13 -0300

    Removed all css

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,9 @@
-import './Hero.css'
-
 export default function Hero() {
 
     return (
-        <div className={"hero"}>
-            <div className={"hero-text-container"}>
-                <p className={"hero-text"}>
+        <div className={"w-full min-h-10 flex items-center justify-center p-20"}>
+            <div className={"flex bg-slate-900/50 p-10"}>
+                <p className={"text-8xl text-center max-w-5xl"}>
                     Do you want to build <strong>efficient</strong> software that <strong>scales</strong>?
                 </p>
             </div>
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,16 +1,55 @@
-import './Navbar.scss'
-import logo from '../../assets/logo.png'
+interface NavLinkProps {
+    href: string;
+    text: string;
+}
+
+function NavLink(props: NavLinkProps) {
+
+    return (
+        <li className={"flex items-center justify-center"}>
+            <a className={"self-center justify-self-center text-xl m-0.5 transition-all duration-500 " +
+                "hover:text-2xl hover:m-0 hover:font-semibold " +
+                "after:content-[''] after:block after:m-auto after:w-0 after:h-1 after:transition-all duration-500 " +
+                "hover:after:w-full hover:after:bg-white hover:after:rounded-full hover:after:mt-1.5 hover:after:mb-0.5"
+            }
+               href={props.href}>{props.text}
+            </a>
+        </li>
+    )
+
+}
+
+function Icon() {
+
+    return (
+        <div className={"flex flex-row items-center"}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="h-20 stroke-white">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"/>
+            </svg>
+            <div>
+                <h1 className={"text-3xl"}>Bruno Lemus</h1>
+                <h2>Functional Programming Evangelist</h2>
+            </div>
+        </div>
+    )
+
+}
 
 export default function Navbar() {
 
     return (
-        <div id={"navbar"}>
-            <img src={logo} alt={"logo"} className={"logo"}/>
-            <ul>
-                <li><a href={'#'}>Home</a></li>
-                <li><a href={'#'}>Technologies</a></li>
-                <li><a href={'#'}>Experience</a></li>
-                <li><a href={'#'}>Contact</a></li>
+        <div
+            className={"pl-10 pt-5 flex justify-between " +
+                "items-center bg-gradient-to-b from-black to-transparent text-lg"}
+        >
+            <Icon/>
+            <ul className={"flex flex-1 justify-around items-center h-auto"}>
+                <NavLink href={"#"} text={"Home"}/>
+                <NavLink href={"#"} text={"Experience"}/>
+                <NavLink href={"#"} text={"Technologies"}/>
+                <NavLink href={"#"} text={"Contact"}/>
             </ul>
         </div>
     )
