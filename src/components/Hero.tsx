43e32e62cebed8ef2ba4acf280fa253ec0ea8f8a
bruno.lemus <bruno.lemus@globant.com>
2023-05-14 22:46:49 -0300

    Add Hero animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,16 @@ export default function Hero() {
     return (
         <div className={"w-full min-h-10 flex items-center justify-center p-20"}>
             <div className={"flex bg-slate-900/50 p-10 max-w-6xl md:flex-row flex-col"}>
-                <p className={"hidden md:block text-6xl text-right max-w-5xl border-gray-300 border-r-2 pr-5"}>
+                <p className={"hidden md:block text-6xl text-right max-w-5xl animate-fadeRight"}>
                     Do you want to build <strong>efficient</strong> software that <strong>scales</strong>?
                 </p>
-                <div className={"flex flex-col content-center justify-center ml-5"}>
+                <div className={"flex-grow-0 border-gray-300 border-r-2 pr-5 animate-expand"}/>
+                <div className={"flex flex-col content-center justify-center ml-5 animate-fadeDown"}>
                     <h1 className={"text-6xl"}>Hello there, I'm Bruno.</h1>
                     <p className={"text-lg"}>
                         I'm a software engineer with a passion for big scale, highly concurrent distributed systems.
-                        I strongly believe the best way to build them is by leveraging the power of functional programming.
+                        I strongly believe the best way to build them is by leveraging the power of functional
+                        programming.
                     </p>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: "class",
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                expand: 'expand 2s ease-in-out',
+                fadeDown: 'fadeDown 2s ease-in-out',
+                fadeRight: 'fadeRight 2s ease-in-out',
+            },
+
+            keyframes: {
+                expand: {
+                    '0%': {transform: 'scaleY(0)', opacity: '0'},
+                    '100%': {transform: 'scaleY(1)', opacity: '1'},
+                },
+                fadeDown: {
+                    '0%': {opacity: '0', transform: 'translateY(-50px)'},
+                    '100%': {opacity: '1', transform: 'translateY(0)'},
+                },
+                fadeRight: {
+                    '0%': {opacity: '0', transform: 'translateX(-50px)'},
+                    '100%': {opacity: '1', transform: 'translateX(0)'},
+                }
+            },
+        },
+    },
+    plugins: [],
 }
 
