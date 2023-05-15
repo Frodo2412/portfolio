89ea03aff7cd72e67df72ca8169176aae6a71897
bruno.lemus <bruno.lemus@globant.com>
2023-05-14 22:52:42 -0300

    Changed icon for profile pic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ interface NavLinkProps {
     text: string;
 }
 
+const profilePicLink = "https://media.licdn.com/dms/image/C4E03AQGXuUN8kAhrPg/profile-displayphoto-shrink_400_400/0/1643247705278?e=1689811200&v=beta&t=EceH8eFRZzRdj6iwGYsGlYt-71G0Ikrc77fPcQNBs0w"
+
 function NavLink(props: NavLinkProps) {
 
     return (
@@ -23,11 +25,11 @@ function Icon() {
 
     return (
         <div className={"flex flex-row items-center"}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                 stroke="currentColor" className="h-20 stroke-white">
-                <path strokeLinecap="round" strokeLinejoin="round"
-                      d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"/>
-            </svg>
+            <img
+                src={profilePicLink}
+                alt={"profile"}
+                className={"rounded-full h-20 w-20 border-2 border-white mr-5"}
+            />
             <div>
                 <h1 className={"text-3xl"}>Bruno Lemus</h1>
                 <h2>Functional Programming Evangelist</h2>
